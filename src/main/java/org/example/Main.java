0b2593e1f7ac79dee6fa4ca439f8336eaceb7884
Apurv Upreti <vontuguna@gmail.com>
2024-02-04 22:52:12 +0530

    reduce in rdd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,10 +21,13 @@ public class Main
         List<Double> inputData = new ArrayList<>();
         inputData.add(35.5);
         inputData.add(90.5);
-        Logger.getLogger("org.apahe").setLevel(Level.WARN);
         SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]"); // config for spark set here
         JavaSparkContext sc = new JavaSparkContext(conf); // used to talk to spark cluster
         JavaRDD<Double> myRdd = sc.parallelize(inputData); // JavaRDD is representation/wrapper of RDD which is originally in scala.
+        Double result = myRdd.reduce((val1, val2) -> val1 + val2 );
+        // spark driver sends this function to all nodes and it is applied on any of the two values in the cluster.
+        // Once only one value is left in cluster then it does a shuffling and sends all data into a single cluster and applies the finction agains
+        System.out.println("result-> " + result);
         sc.close();
 
     }
